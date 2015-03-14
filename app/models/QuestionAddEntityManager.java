4cdba1f7ad66f2987e1dbb0b69d2ddeea6b7bd57
Sanket Kumar <sanketsloth@gmail.com>
2015-03-15 00:18:33 +0530

    Deleted persistence file.

--- app/models/QuestionAddEntityManager.java
+++ app/models/QuestionAddEntityManager.java
@@ -6,39 +6,20 @@ import controllers.Question;
 import controllers.Utilities;
 
 public class QuestionAddEntityManager {
-	@SuppressWarnings("unused")
-	public static void addQuestion() {
-		Handle h = null;
-		try {
-			h = BaseEntityManager.getDBIh();
-
-			QuestionDao dao;
-			dao = h.attach(QuestionDao.class);
-			h.begin();
-			dao.addQuestion(Question.getQuestionID(), Question.getQuestion(),
-					Question.getDescription(), Question.isAnonymous());
-			for (String tag : Question.getTags()) {
-				dao.addTags(Utilities.generateUUID(), Question.getQuestionID(),
-						tag);
-			}
-			for (String option : Question.getOptions()) {
-				dao.addAnswers(Utilities.generateUUID(),
-						Question.getQuestionID(), option);
-			}
-			h.commit();
-			h.close();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			if (h != null) {
-				h.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (h != null) {
-				h.close();
-			}
+	public static void addQuestion() throws ClassNotFoundException {
+		Handle h = BaseEntityManager.getDBIh();
+		QuestionDao dao;
+		dao = h.attach(QuestionDao.class);
+		h.begin();
+		dao.addQuestion(Question.getQuestionID(), Question.getQuestion(),
+				Question.getDescription(), Question.isAnonymous());
+		for(String tag: Question.getTags()){
+			dao.addTags(Utilities.generateUUID(), Question.getQuestionID(), tag);
 		}
-		
+		for(String option: Question.getOptions()) {
+			dao.addAnswers(Utilities.generateUUID(), Question.getQuestionID(), option);
+		}
+		h.commit();
+		h.close();
 	}
 }
--- build.sbt
+++ build.sbt
@@ -6,13 +6,8 @@ libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
   cache,
-  javaJpa,
   "mysql" % "mysql-connector-java" % "5.1.21",
   "org.json"%"org.json"%"chargebee-1.0",
-  "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final",
-  "org.hibernate" % "hibernate-core" % "3.5.6-Final",
-  "org.hibernate" % "hibernate-annotations" % "3.5.6-Final",
-  "joda-time" % "joda-time-hibernate" % "1.3",
   "org.jdbi" % "jdbi" % "2.59"
 )     
 
