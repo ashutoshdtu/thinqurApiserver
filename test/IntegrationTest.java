a57cfbcce594573bf54fdac39f9f230bab871f64
Ashutosh Mishra <ashutoshdtu@gmail.com>
2015-06-16 07:01:30 +0530

    update integration test

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,12 +1,11 @@
 import org.junit.*;
 
+import play.Play;
 import play.mvc.*;
 import play.test.*;
 import play.libs.F.*;
-
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
-
 import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
@@ -20,7 +19,7 @@ public class IntegrationTest {
         running(testServer(9000, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:9000");
-                assertThat(browser.pageSource()).contains("Welcome to our API Server");
+                assertThat(browser.pageSource()).contains("Welcome to Thinqur API Server");
             }
         });
     }
