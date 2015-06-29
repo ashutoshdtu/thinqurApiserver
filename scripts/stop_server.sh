d22896d720b1352c92490fe58f7ed252c8e2bc82
Ashutosh Mishra <ashutoshdtu@gmail.com>
2015-06-29 17:58:21 +0530

    updating stop_server script

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,6 +3,6 @@ echo "Stopping apiserver"
 
 #cmd="cd .."
 PORT_NUMBER=9000
-cmd="lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill "
+cmd="lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print \$2}' | xargs kill "
 echo $cmd
-$cmd
+lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill 
