62f1d1c54271c479cd9f321c1bb7bcff42f11ca6
Sanket Kumar <sanketsloth@gmail.com>
2015-07-01 04:40:01 +0530

    Added logging for api calls.

--- app/Global.java
+++ app/Global.java
@@ -1,9 +1,12 @@
+import java.lang.reflect.Method;
 
 import play.Application;
 import play.GlobalSettings;
 import play.Logger;
+import play.Logger.ALogger;
+import play.mvc.Action;
+import play.mvc.Http.Request;
 import utils.DAOUtils;
-
 
 /**
  * @author ashutosh
@@ -11,14 +14,20 @@ import utils.DAOUtils;
  */
 public class Global extends GlobalSettings {
 
-	    public void onStart(Application app) {
-	        Logger.info("Application started!");
-	        DAOUtils.connect();
-	    }
+	public void onStart(Application app) {
+		Logger.info("Application started!");
+		DAOUtils.connect();
+	}
 
-	    public void onStop(Application app) {
-	        Logger.info("Appplication stopped!");
-	        DAOUtils.disconnect();
-	    }
-	    
+	public void onStop(Application app) {
+		Logger.info("Appplication stopped!");
+		DAOUtils.disconnect();
+	}
+
+	@SuppressWarnings("rawtypes")
+	public Action onRequest(Request request, Method method) {
+		Logger.info("method=" + request.method() + "   uri=" + request.uri()
+				+ "   remote-address=" + request.remoteAddress());
+		return super.onRequest(request, method);
+	}
 }
